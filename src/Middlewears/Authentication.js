d3d283cf913f82f04d7cc90e1bc46dd566215e95
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-11-12 09:48:15 +0530

    Update Authentication.js

--- src/Middlewears/Authentication.js
+++ src/Middlewears/Authentication.js
@@ -7,7 +7,7 @@ const token=req.headers["x-access-token"]
 if(!token)
 {
     return res.status(400).json({
-        success:false,
+        success:false, 
         message:'Token is required'
     })
 }
